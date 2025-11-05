f54c96c364a53d98e3af5565a05692b67024b46e
Joseph Pal <josephpal27@gmail.com>
2025-11-05 17:14:41 +0530

    Features section added

--- src/components/home-welcome/HomeWelcome.jsx
+++ src/components/home-welcome/HomeWelcome.jsx
@@ -19,10 +19,20 @@ const HomeWelcome = () => {
             <h2>We’re Committed <br /> To quality</h2>
             <p>We are experts in providing superior products at a lower cost, and we have worked on Metal, Refinery, and Power projects. Our steel goods are of great quality, and we have a good reputation with our clients. I believe this is a nice start to a long-term relationship.</p>
             <div className="features">
-                <p><FaCheck className="icon" /> We provide 24/7 service</p>
-                <p><FaCheck className="icon" /> We strick to deadline</p>
-                <p><FaCheck className="icon" /> We offer upfront pricing</p>
-                <p><FaCheck className="icon" /> We use latest technology</p>
+                <div className="features-left">
+                    <p><FaCheck className="icon" /> We provide 24/7 service</p>
+                    <p><FaCheck className="icon" /> We strick to deadline</p>
+                    <p><FaCheck className="icon" /> We offer upfront pricing</p>
+                    <p><FaCheck className="icon" /> We use latest technology</p>
+                </div>
+                <div className="features-right">
+                    <div className="feature-box">
+                        <p>Welding joins together</p>
+                    </div>
+                    <div className="feature-box">
+                        <p>Have all metal works tools</p>
+                    </div>
+                </div>
             </div>
             <div className="tagline">
                 <p>WELDER PRO PROFESSIONAL WELDING COMPANY IN INDIA</p>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,7 +2,7 @@
 const Error = () => {
   return (
     <>
-    <div style={{textAlign: "center", backgroundColor: '#e6e6e6ff', height: '100vh'}}>
+    <div style={{textAlign: "center", backgroundColor: '#ffb01dff', height: '100vh'}}>
         <h1 style={{paddingTop: '45vh', fontSize: '5rem', fontWeight: '700'}}>PAGE NOT FOUND</h1>
     </div>
     </>
