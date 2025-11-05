0829f2ec72927d265779d843e10c67f94adf962e
Joseph Pal <josephpal27@gmail.com>
2025-11-05 16:57:48 +0530

    error page updated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,8 +2,8 @@
 const Error = () => {
   return (
     <>
-    <div style={{textAlign: "center"}}>
-        <h1 style={{marginTop: '30vh', fontSize: '7rem', fontWeight: '700'}}>PAGE NOT FOUND</h1>
+    <div style={{textAlign: "center", backgroundColor: '#e6e6e6ff', height: '100vh'}}>
+        <h1 style={{paddingTop: '45vh', fontSize: '5rem', fontWeight: '700'}}>PAGE NOT FOUND</h1>
     </div>
     </>
   )
