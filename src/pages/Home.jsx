923f8601aa281a465566a415138e07cd2f5e0d5b
Joseph Pal <josephpal27@gmail.com>
2025-11-05 18:14:20 +0530

    what we offer updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import '../css/Home.css';
 
 import homeBanner from '/images/banners/banner.avif';
+
 import HomeWelcome from '../components/home-welcome/HomeWelcome';
 import CTA from '../components/cta/CTA';
 
@@ -17,6 +18,17 @@ const Home = () => {
 
       {/* CTA */}
       <CTA/>
+
+      {/* What We Offer */}
+      <section className="what-we-offer">
+          <div className="what-we-offer-head">
+            <span>Our Services</span>
+            <h3>What we offer</h3>
+          </div>
+          <div className="what-we-offer-cards-row">
+            
+          </div>
+      </section>
     </>
   )
 }
