ca2628b1dbf5eaa51f08575ff955cc25b95b43ab
Joseph Pal <josephpal27@gmail.com>
2025-11-06 13:30:48 +0530

    Testimonial tabbing system added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import homeBanner from '/images/banners/banner.avif';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+import { Tab, Nav } from "react-bootstrap";
+
 import HomeWelcome from '../components/home-welcome/HomeWelcome';
 import CTA from '../components/cta/CTA';
 
@@ -15,6 +17,9 @@ import ServiceImage3 from '../assets/images/services/3.avif';
 import testimonialIcon1 from '../assets/images/icons/work.png';
 import testimonialIcon2 from '../assets/images/icons/welding.png';
 import testimonialIcon3 from '../assets/images/icons/welding-machine.png';
+
+import testimonialUserM from '../assets/images/icons/user-m.png';
+import testimonialUserF from '../assets/images/icons/user-f.png';
 
 const Home = () => {
 
@@ -80,36 +85,97 @@ const Home = () => {
 
       {/* Testimonials */}
       <section className="testimonials">
-          <div className="testimonials-left">
-            <span>Our Testimonials</span>
-            <h4>WHAT THEY SAY</h4>
+        <div className="testimonials-left">
+          <span>Our Testimonials</span>
+          <h4>WHAT THEY SAY</h4>
+          <div className="testimonials-tabbing">
+            <Tab.Container defaultActiveKey="tabOne">
+              {/* Tabs Navigation */}
+              <Nav variant="tabs" className="tab-head">
+                <Nav.Item>
+                  <Nav.Link eventKey="tabOne">
+                    <img src={testimonialUserM} alt="Icon" loading="lazy" />
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="tabTwo">
+                    <img src={testimonialUserM} alt="Icon" loading="lazy" />
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="tabThree">
+                    <img src={testimonialUserF} alt="Icon" loading="lazy" />
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="tabFour">
+                    <img src={testimonialUserM} alt="Icon" loading="lazy" />
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="tabFive">
+                    <img src={testimonialUserF} alt="Icon" loading="lazy" />
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+
+              {/* Tabs Content */}
+              <Tab.Content>
+                <Tab.Pane eventKey="tabOne" className="fade show">
+                  <p>I have just had two of the most productive work weeks I have had in years. Ben was perfectly matched to my temperament, and has been taking all the backlogged work off my plate and finishing it.</p>
+                  <span className="client-name">Jessica Brown</span>
+                  <span className="client-designation">Co-Founder</span>
+                </Tab.Pane>
+                <Tab.Pane eventKey="tabTwo" className="fade">
+                  <p>I have just had two of the most productive work weeks I have had in years. Ben was perfectly matched to my temperament all the backlogged work off my plate and finishing.</p>
+                  <span className="client-name">Jessica Brown</span>
+                  <span className="client-designation">Co-Founder</span>
+                </Tab.Pane>
+                <Tab.Pane eventKey="tabThree" className="fade">
+                  <p>I have just had two of the most productive work weeks I have had in years. Ben was perfectly matched to my temperament, and has been taking all the backlogged work off my plate and finishing it.</p>
+                  <span className="client-name">Jessica Brown</span>
+                  <span className="client-designation">Co-Founder</span>
+                </Tab.Pane>
+                <Tab.Pane eventKey="tabFour" className="fade">
+                  <p>I have just had two of the most productive work weeks I have had in years. Ben was perfectly matched to my temperament all the backlogged work off my plate and finishing.</p>
+                  <span className="client-name">Jessica Brown</span>
+                  <span className="client-designation">Co-Founder</span>
+                </Tab.Pane>
+                <Tab.Pane eventKey="tabFive" className="fade">
+                  <p>I have just had two of the most productive work weeks I have had in years. Ben was perfectly matched to my temperament, and has been taking all the backlogged work off my plate and finishing it.</p>
+                  <span className="client-name">Jessica Brown</span>
+                  <span className="client-designation">Co-Founder</span>
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </div>
-          <div className="testimonials-right">
-            <div className="testimonial-hightlight-box">
-              <div className="hightlight-image">
-                <img src={testimonialIcon1} alt="Icon" loading="lazy" />
-              </div>
-              <div className="hightlight-content">
-                <p>We’re Good in All Metal Works</p>
-              </div>
+        </div>
+        <div className="testimonials-right">
+          <div className="testimonial-hightlight-box">
+            <div className="hightlight-image">
+              <img src={testimonialIcon1} alt="Icon" loading="lazy" />
             </div>
-            <div className="testimonial-hightlight-box">
-              <div className="hightlight-image">
-                <img src={testimonialIcon2} alt="Icon" loading="lazy" />
-              </div>
-              <div className="hightlight-content">
-                <p>Expert & Professional Welder Workers</p>
-              </div>
-            </div>
-            <div className="testimonial-hightlight-box">
-              <div className="hightlight-image">
-                <img src={testimonialIcon3} alt="Icon" loading="lazy" />
-              </div>
-              <div className="hightlight-content">
-                <p>We’re Using Quality Welding Tools</p>
-              </div>
+            <div className="hightlight-content">
+              <p>We’re Good in All Metal Works</p>
             </div>
           </div>
+          <div className="testimonial-hightlight-box">
+            <div className="hightlight-image">
+              <img src={testimonialIcon2} alt="Icon" loading="lazy" />
+            </div>
+            <div className="hightlight-content">
+              <p>Expert & Professional Welder Workers</p>
+            </div>
+          </div>
+          <div className="testimonial-hightlight-box">
+            <div className="hightlight-image">
+              <img src={testimonialIcon3} alt="Icon" loading="lazy" />
+            </div>
+            <div className="hightlight-content">
+              <p>We’re Using Quality Welding Tools</p>
+            </div>
+          </div>
+        </div>
       </section>
 
     </>
