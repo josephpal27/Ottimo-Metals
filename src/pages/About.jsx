966ca7acbcc820e059c5d5fbfaf308269803e840
Joseph Pal <josephpal27@gmail.com>
2025-11-07 17:45:29 +0530

    Our Products added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,9 @@ import ProductDetails from '../components/product-details/ProductDetails';
 import '../css/About.css';
 
 import homeBanner from '/images/banners/banner.avif';
+import ourProductImage1 from '../assets/images/products/1.avif';
+import ourProductImage2 from '../assets/images/products/2.avif';
+import ourProductImage3 from '../assets/images/products/3.avif';
 
 const About = () => {
     return (
@@ -15,13 +18,32 @@ const About = () => {
             </section>
 
             {/* About Welcome */}
-            <AboutWelcome/>
+            <AboutWelcome />
 
             {/* Mission Vision */}
-            <MissionVision/>
+            <MissionVision />
 
             {/* Product Details */}
-            <ProductDetails/>
+            <ProductDetails />
+
+            {/* Our Products */}
+            <section className="our-products">
+                <div className="our-products-head">
+                    <h4>OUR PRODUCTS</h4>
+                    <div className="line"></div>
+                </div>
+                <div className="our-products-boxes-row">
+                    <div className="our-products-box">
+                        <img src={ourProductImage1} alt="Product Image" loading="lazy" />
+                    </div>
+                    <div className="our-products-box">
+                        <img src={ourProductImage2} alt="Product Image" loading="lazy" />
+                    </div>
+                    <div className="our-products-box">
+                        <img src={ourProductImage3} alt="Product Image" loading="lazy" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
