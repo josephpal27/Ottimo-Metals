aa1a2554d12fbc3595ae55dad7d13c693f34becf
Joseph Pal <josephpal27@gmail.com>
2025-11-08 14:01:15 +0530

    What we do added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import '../css/Products.css';
 
 import homeBanner from '/images/banners/banner.avif';
+import whatWeDoImage from '../assets/images/products/what-we-do.avif';
 
 const Products = () => {
     return (
@@ -8,6 +9,18 @@ const Products = () => {
             {/* Banner */}
             <section className="banner">
                 <img src={homeBanner} alt="Home Banner" loading="eager" />
+            </section>
+
+            {/* What We Do */}
+            <section className="what-we-do">
+                <div className="what-we-do-content">
+                    <h2>WHAT WE DO</h2>
+                    <p>Ottimo Metals takes pride in its expertise in manufacturing high purity zinc wires drawn from high grade zinc ingots(Zn: 99.995%) that have earned a reputation for exceptional quality and unwavering consistency. Made from 99.995% pure Zinc, our wires are meticulously crafted to cater to a wide range of applications, guaranteeing superior performance and uniformity in every product.</p>
+                </div>
+                <div className="what-we-do-image">
+                    <img src={whatWeDoImage} alt="What we do" loading="lazy" />
+                    <div className="layer"></div>
+                </div>
             </section>
         </>
     )
