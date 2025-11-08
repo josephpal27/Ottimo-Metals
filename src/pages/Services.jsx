ba5a5075f1fdd07181ce480a2858a6628358d460
Joseph Pal <josephpal27@gmail.com>
2025-11-08 12:37:38 +0530

    Services Page updated

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,69 @@
 import '../css/Services.css';
 
+import homeBanner from '/images/banners/banner.avif';
+
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import serviceImage1 from '../assets/images/services/4.avif';
+import serviceImage2 from '../assets/images/services/2.avif';
+import serviceImage3 from '../assets/images/services/3.avif';
+
 const Services = () => {
-  return (
-    <>
-      
-    </>
-  )
+
+    let serviceCardsData = [
+        {
+            id: 1,
+            image: serviceImage1,
+            title: "Turnkey Projects",
+            desc: "We undertake comprehensive corrosion protection projects for a wide range of applications, including structural steel, pipes, castings, steel fabrications, cylinders, and other cast/steel products. Our expertise ensures thorough protection and extended durability for your valuable assets.",
+        },
+        {
+            id: 2,
+            image: serviceImage2,
+            title: "Consultancy",
+            desc: "Our experienced team offers detailed studies and expert consultation to provide customized solutions tailored to your requirements. We analyze your products and provide professional recommendations to effectively build structures, prevent corrosion and extend the lifespan of various constructions.",
+        },
+        {
+            id: 3,
+            image: serviceImage3,
+            title: "Technical Support",
+            desc: "Our commitment to delivering utmost customer satisfaction drives us to provide comprehensive technical support for our manufactured products. Our proficient team is always prepared to address your inquiries, ensuring you receive unparalleled guidance and assistance for all your zinc wire requirements. We are dedicated to enhancing wire efficiency and are here to help you optimize your usage and experience.",
+        },
+    ]
+
+    return (
+        <>
+            {/* Banner */}
+            <section className="banner">
+                <img src={homeBanner} alt="Home Banner" loading="eager" />
+            </section>
+
+            {/* Services */}
+            <section className="offered-services">
+                <p>At <span>Ottimo Metals</span>, we are proud to be experts in manufacturing high-quality zinc wires and providing specialized services such as consultancy and customized project development services. Our primary focus is on harnessing the potential of zinc wires to cater to various industries and projects.</p>
+                <b>Here are the services we offer:</b>
+                <div className="offered-services-row">
+                    {
+                        serviceCardsData.map((item, index) => {
+                            return (
+                                <div className="offered-services-card" key={index}>
+                                    <div className="card-img">
+                                        <img src={item.image} alt="Service Image" loading="lazy" />
+                                    </div>
+                                    <div className="card-desc">
+                                        <span>{item.title}</span>
+                                        <p>{item.desc}</p>
+                                        <Button component={Link} to="" className="button">EXPLORE</Button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Services
