225f3ed2234f6ebfa8373ed89eb37c8a95f437fc
Joseph Pal <josephpal27@gmail.com>
2025-11-14 15:08:14 +0530

    Application Images updated

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -2,6 +2,14 @@ import '../css/Applications.css';
 
 import homeBanner from '/images/banners/banner.avif';
 import appsImg1 from '../assets/images/applications/1.avif';
+import appsImg2 from '../assets/images/applications/2.avif';
+import appsImg3 from '../assets/images/applications/3.avif';
+import appsImg4 from '../assets/images/applications/4.avif';
+import appsImg5 from '../assets/images/applications/5.avif';
+import appsImg6 from '../assets/images/applications/6.avif';
+import appsImg7 from '../assets/images/applications/7.avif';
+import appsImg8 from '../assets/images/applications/8.avif';
+import appsImg9 from '../assets/images/applications/9.avif';
 
 const Applications = () => {
 
@@ -14,49 +22,49 @@ const Applications = () => {
         },
         {
             id: 2,
-            image: appsImg1,
+            image: appsImg2,
             title: "Wind Power Plants",
             desc: "Zinc wiring empowers wind power plants by fortifying turbine structures, improving corrosion resistance, and maximizing energy production for sustainable, clean power generation.",
         },
         {
             id: 3,
-            image: appsImg1,
+            image: appsImg3,
             title: "Ductile Iron Pipes",
             desc: "Zinc wiring acts as a protective shield, safeguarding ductile iron pipes against rust, corrosion, and extending their lifespan in demanding environments.",
         },
         {
             id: 4,
-            image: appsImg1,
+            image: appsImg4,
             title: "Steel Fabrications",
             desc: "By integrating zinc wiring, steel fabrications achieve enhanced durability, anti-corrosion properties, and prolonged structural integrity, enabling versatile applications.",
         },
         {
             id: 5,
-            image: appsImg1,
+            image: appsImg5,
             title: "Steel Pipes",
             desc: "Zinc wiring forms robust barrier, shielding steel pipes from corrosion, rust, and harsh environmental conditions, ensuring long-lasting performance and reliability.",
         },
         {
             id: 6,
-            image: appsImg1,
+            image: appsImg6,
             title: "Rail & Road Bridges",
             desc: "Zinc wiring reinforces rail and road bridges, bolstering their load-bearing capacity, resilience against corrosion, and ensuring safe and reliable transportation infrastructure.",
         },
         {
             id: 7,
-            image: appsImg1,
+            image: appsImg7,
             title: "Tankers",
             desc: "Zinc wiring protects tankers against corrosion, extending their service life, and ensuring the safe transportation of valuable liquids and gases across vast distances.",
         },
         {
             id: 8,
-            image: appsImg1,
+            image: appsImg8,
             title: "Finned Tube",
             desc: "Zinc wire thermal metallization serves as a robust protective shield, effectively warding off rust and corrosion. This application significantly prolongs the lifespan of various materials, ensuring their durability even in the harshest weather conditions.",
         },
         {
             id: 9,
-            image: appsImg1,
+            image: appsImg9,
             title: "Ship Building & Repairing",
             desc: "Zinc wiring revolutionizes ship construction and repair processes, enabling corrosion-resistant vessels, enhanced structural integrity, and efficient maintenance in marine environments.",
         },
