545093a6b9aa80f7813a3795d03cef72ed7cb33f
Joseph Pal <josephpal27@gmail.com>
2025-11-14 16:34:47 +0530

    Quality control content updated

--- src/pages/QualityAssurance.jsx
+++ src/pages/QualityAssurance.jsx
@@ -1,6 +1,7 @@
 import '../css/QualityAssurance.css';
 
 import homeBanner from '/images/banners/banner.avif';
+import qualityControlImg from '../assets/images/quality-control.avif';
 
 import { FaCheck } from "react-icons/fa6";
 
@@ -13,7 +14,7 @@ const QualityAssurance = () => {
             </section>
 
             {/* Quality Assurance */}
-            <div className="quality-assurance">
+            <section className="quality-assurance">
                 <h2>ZINC WIRES</h2>
                 <div className="line"></div>
                 <div className="quality-cards-row">
@@ -36,7 +37,24 @@ const QualityAssurance = () => {
                         <p>From zinc ingots to drawing and packaging zinc wires, our manufacturing process is fully automated.</p>
                     </div>
                 </div>
-            </div>
+            </section>
+
+            {/* Quality Control */}
+            <section className="quality-control">
+                <div className="quality-control-content">
+                    <h4>QUALITY CONTROL</h4>
+                    <p>At Ottimo Metals, our unwavering commitment to quality is upheld through a rigorous and comprehensive Quality Control plan. This plan is overseen by a team of adept engineers with technical expertise, complemented by a highly trained and qualified staff.</p>
+                    <ul>
+                        <li>ISO 9001:2015 Quality System and ISO 14001:2015 Environment Management System. SHG grade zinc (Zn 99.995%) adhering to DIN EN 14919 standards.</li>
+                        <li>Our modern in-house laboratory, outfitted with cutting-edge German technology, boasts a Photo Spectrometer that meticulously verifies chemical compositions.</li>
+                        <li>Additionally, we leverage a European standard UTM Machine, coupled with a Metallurgical microscope, to scrutinize the mechanical and physical attributes of our products.</li>
+                    </ul>
+                    <p>This meticulous approach serves as a distinct advantage, ensuring that our offerings consistently meet and exceed the highest standards of excellence.</p>
+                </div>
+                <div className="quality-control-image">
+                    <img src={qualityControlImg} alt="Quality Control" loading="lazy" />
+                </div>
+            </section>
         </>
     )
 }
