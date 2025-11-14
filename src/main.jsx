a8e71fb3fb430fd3f93b3396e8a0cab3d63ed6bf
Joseph Pal <josephpal27@gmail.com>
2025-11-14 12:58:34 +0530

    Gallery Page Init

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import About from './pages/About.jsx';
 import Services from './pages/Services.jsx';
 import Products from './pages/Products.jsx';
 import Applications from './pages/Applications.jsx';
+import Gallery from './pages/Gallery.jsx';
 import Error from './pages/Error.jsx';
 
 let routers = createBrowserRouter([
@@ -23,6 +24,7 @@ let routers = createBrowserRouter([
       {path: "/services", element: <Services />},
       {path: "/products", element: <Products />},
       {path: "/applications", element: <Applications />},
+      {path: "/gallery", element: <Gallery />},
       {path: "*", element: <Error />},
     ]
   }
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,11 @@
+import '../css/Gallery.css';
+
+const Gallery = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Gallery
